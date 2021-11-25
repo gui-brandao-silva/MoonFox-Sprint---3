e1c46c0f00a4cc691766e542a2578d2cefed557c
danielamrl <daniel.silva@bandtec.com.br>
2021-11-25 09:51:05 -0300

    api_att.att

---
+++ BD_Banco De Dados/script_3_0.sql
@@ -0,0 +1,67 @@
+create database bd_moonfox;
+use bd_moonfox;
+
+/* TABELA EMPRESA */
+create table tb_Empresa (
+id_empresa int primary key auto_increment,
+Nome_Empresa varchar(45),
+CNPJ_CPF varchar(14),
+EndereçoCliente varchar(45),
+CEP varchar(45),
+Data_abertura date
+);
+select*from empresa;
+
+/* TABELA USUARIO */
+create table tb_usuario(
+id_usuario int auto_increment ,
+fk_empresa int ,
+Nome varchar(45),
+Email varchar(45),
+Senha varchar(45),
+ PRIMARY KEY (`id_usuario`, `fk_empresa`),
+foreign key (fk_empresa) references tb_Empresa (id_empresa)
+)auto_increment = 10;
+select * from usuario;
+
+/* TABELA FILIAIS */
+create table tbs_filiais (
+id_filiais int auto_increment,
+fk_empresa int,
+NomeFilial varchar(45),
+Quantidade_de_Salas int,
+CEP_filial varchar(45),
+PRIMARY KEY (`id_filiais`, `fk_empresa`),
+foreign key (fk_empresa) references tb_Empresa (id_empresa)
+)auto_increment = 20;
+select * from tb_filiais;
+
+/* TABELA IMPRESSORA */
+create table tb_impressora (
+id_impressora int primary key auto_increment,
+ModeloImpressora varchar(45),
+Filamento varchar (3),
+check( Filamento = 'ABS' or Filamento = 'PLA'),
+Altura_Z decimal (10,2),
+Largura_X decimal (10,2),
+profundidade_Y decimal (10,2),
+fk_filiais int,
+foreign key (fk_filiais) references tbs_filiais (id_filiais)
+)auto_increment = 30;
+select * from tbs_impressora;
+
+/* TABELA SENSOR */
+create table tb_sensor(
+id_sensor int primary key auto_increment,
+fk_impressora int,
+foreign key (fk_impressora) references tb_impressora (id_impressora)
+)auto_increment = 40;
+select * from sensor;
+
+create table tb_registros(
+id_registro int auto_increment,
+fk_sensor int,
+primary key (`id_registro`,`fk_sensor`),
+Temperatura_Atual decimal (10,2)
+)auto_increment = 50;
+select * from tb_registros;
