18268844813d7ad9d10a277470ab9c2b04caff0d
EnzoFBS <enzo.silvestre@bandtec.com.br>
2021-12-04 23:15:29 -0300

    metrica_60%

--- Api-grafico/moonfox-api/api-site/src/models/medidaModel.js
+++ Api-grafico/moonfox-api/api-site/src/models/medidaModel.js
@@ -1,23 +1,23 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas( limite_linhas) {
+function buscarUltimasMedidas( id_sensor,limite_linhas) {
     instrucaoSql = `select 
                         temperatura, 
                         momento,
                         DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
                     from tb_registros
-                    where fk_sensor = ${40}
-                    order by id_registro desc limit ${limite_linhas}`;
+                    where fk_sensor = ${id_sensor}
+                    order by id_registro desc limit ${id_sensor,limite_linhas}`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal() {
+function buscarMedidasEmTempoReal(id_sensor) {
     instrucaoSql = `select 
                         temperatura, 
                          DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
                         fk_sensor 
-                        from tb_registros where fk_sensor = ${40} 
+                        from tb_registros where fk_sensor = ${id_sensor} 
                     order by id_registro desc limit 1`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
