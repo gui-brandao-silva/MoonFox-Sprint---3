f1653e7debd77e5703059c2b9855e6cd4267d209
lucas-bazilio <lucas.gsilva@bandtec.com.br>
2021-12-16 00:14:56 -0300

    dados postos faltam as tabelas associativas

--- BEN 100000000000000000/Ben10att.sql
+++ BEN 100000000000000000/Ben10att.sql
@@ -8,7 +8,28 @@ nome varchar (45),
 raça varchar(45),
 nível varchar(45)
 );
-insert into inimigo values();
+insert into inimigo (nome,raça,nível) values
+("Vilgax","Chimera Sui Generis","100"),
+("Dr Animal","Humano","55"),
+("Cavaleiros Eternos","Humanos","65"),
+("Zomboso","Mutante","68"),
+("Albedo","Galvaniano","95"),
+("Desentupidores","Aliens","100"),
+("Aggregor","Asmosiano","100"),
+("Encantriz","Humano","70"),
+("HEX","Humano","70"),
+("Dr Viktor","transyliano","100"),
+("Blue","Humano","67"),
+("Estrela Sombria","humano","70"),
+("Irmaos Vreedle","Vreedles","65"),
+("Joseph Chadwick","humano","60"),
+("Billy Billions","Humano","55"),
+("Soberanos","Soberanus","80"),
+("Rei Eterno","Humano","100"),
+("Attea","Incursiano","70"),
+("Looma Vento Vermelho","Tetramando","70"),
+("Capitao N","Humano","40");
+
 select * from inimigo;
 
 -- tabela 2
@@ -17,10 +38,14 @@ idPoder int primary key auto_increment,
 tipo varchar (45),
 destruiçao int
 );
-insert into poder (tipo, destruiçao) values ("Controle de Fogo", 70), ("Controle de Água", 48), ("gelo", 59), ("teletransporte", 78), ("Telecinese", 94),
+insert into poder (tipo, destruiçao) values ("Controle de Fogo", 70), ("Controle de Água", 70),("teletransporte", 78), ("Telecinese", 94),
 ("SuperForça", 61), ("Imortalidade", 75), ("Invisibilidade", 60), ("Camuflagem", 40), ("Garras", 40), ("SuperVelocidade", 85), ("Amorfismo", 50), ("Incorporalidade",50),
-("Controle da Terra", 40), ("Manipulação de Ar", 60), ("Amorfismo", 50), ("Elasticidade", 54), ("Regeneração", 73), ("Manipulação de Eletricidade", 76), 
-("Manipulação de Plantas", 58), ("Controle Mental", 89), ("Telepatia", 58), ("Ilusionismo", 60);
+("Controle da Terra", 40), ("Manipulação de Ar", 60), ("Metamorfagem", 50), ("Elasticidade", 54), ("Regeneração", 73), ("Manipulação de Eletricidade", 76), 
+("Manipulação de Plantas", 58), ("Controle Mental", 89), ("Telepatia", 58), ("Ilusionismo", 60), ("Controle Temporal", 90),("Manipução de Cristais",80),
+("Disparo De gosma",35),("Respirar debaixo da agua",70),("Alta resistencia",70),("Rajada Sonicas",75),("Controle de gravidade",90),("Construção de maquinas",40),
+("Agiliade Extrema",80),("Manipulacao de gases",75),("laminas de lase",80),("Blindagem",90),("Hyper força", 100),("Controle de gelo",70),("Clonagem","80"),("Dominacao galatica",90),
+("Disparos de Energia",60),("Super inteligencia",90),("Metamorfose",70),("indestrutibilidade",90),("Bolas adesivas explosivas",70),("Manipulação de energia interna",70),
+("Manipulação de radiação",90),("Diminuir tamanho",65),("Magnetismo",80);
 select * from poder ;
 
 -- tabela 3
@@ -42,9 +67,11 @@ cor varchar(45),
 criaçao date
 );
 insert into relogio (nome,criador,cor,criacao) values
-("Relógio do poder","Alien X","laranja"),
-("Omnitrix","Azmuth","verde"),
-("Omnitrix-negativo","Myaxx","vermelho");
+("Relógio do poder","Alien X","laranja","1-01-01"),
+("Omnitrix","Azmuth","verde","1000-10-10"),
+("Omnitrix-negativo","Myaxx","vermelho","1200-10-10"),
+("Super-Omnitrix","Albedo","Vermelho-escuro","2009-07-03"),
+("Nemetrix","Dr Psychobos","Vermelho-claro","2011-09-04");
 select * from relogio ;
 
 -- tabela 5
@@ -56,28 +83,67 @@ porte varchar(45),
 perigo int
 );
 insert into alien (nome,raca,porte,perigo)values
-("besta","vulpimancwer","x","x"),
-("Quatro braços","tetramando","x","x"),
-("Massa cinzenta","galvaniano","x","x"),
-("XLR8","kinicelerano","x","x"),
-("Utrt","mecamorfo","x","x"),
-("Diamante","petrosadien","x","x"),
-("Aquatico","pisccis volann","x","x"),
-("Insectoide","lepidopterrano","x","x"),
-("Fantasmático","ectonurita","x","x"),
-("Chama","pyronita","x","x"),
-("Bola de Canhão","pelarota","x","x"),
-("Glutão","floranna","x","x"),
-("Clone","thep","x","x"),
-("Ben-Victor","transyliano","x","x"),
-("Benmúmia","mummyon","x","x"),
-("Lobisben","loboan","x","x"),
-("Cipó selvagem","spixiom","x","x"),
-("Gigante","tolkustar","x","x"),
-("Cuspidor","chroniano","x","x"),
-("Iguana Artíca","nojodeeniano","x","x"),
-("Chocante","polar manzardill","x","x");
-;
+("besta","vulpimancwer","B","D"),
+("Quatro braços","tetramando","A","B"),
+("Massa cinzenta","galvaniano","C","A"),
+("XLR8","kinicelerano","A","B"),
+("Utrt","mecamorfo","A","S"),
+("Diamante","petrosadien","A","A"),
+("Aquatico","pisccis volann","D","D"),
+("Insectoide","lepidopterrano","D","C"),
+("Fantasmatico","ectonurita","A","A"),
+("Chama","pyronita","A","S"),
+("Bola de Canhão","pelarota","A","B"),
+("Glutao","floranna","D","C"),
+("Clone","thep","A","B"),
+("Ben-Victor","transyliano","B","B"),
+("Benmúmia","mummyon","A","A"),
+("Lobisben","loboan","A","B"),
+("Cipo selvagem","spixiom","B","B"),
+("Gigante","tolkustar","S","SS"),
+("Cuspidor","chroniano","B","B"),
+("Iguana Artica","nojodeeniano","A","A"),
+("Chocante","polar manzardill","C","A"),
+("Macaco-Aranha","Arachnachimp","B","A"),
+("Enormossauro","Vaxasauriano","S","S"),
+("Friagem","Necrofrigiano","A","S"),
+("Fogo-Fatuo","metanosiano","A","A"),
+("Gosma","Viscosia","A","A"),
+("Artropode","Cerebrocrustaceano","B","S"),
+("Cromático","Crsytalsadiano","A","A"),
+("Eco-Eco","Sonorosiano","A","B"),
+("Arraia-Ajato","Aerofibiano","A","B"),
+("Alien-X","Sapien Celestial","SS","SS"),
+("Estrela-Polar","Biosovortiano","A","A"),
+("Rath","Appoplexiano","A","S"),
+("Nanomech","Nanochip","C","S"),
+("Ameaça-Aquatica","Orishano","A","A"),
+("Tartagira","Geochelone-Aerio","A","B"),
+("Amperi","Amperiano","A","A"),
+("NRG","Prypiatosiano-B","S","S"),
+("Armatu","Talpadeano","A","S"),
+("Camaleao","Viscosia","C","B"),
+("Contratempo","Chronosapien","S","S"),
+("Acelerado","CitraKayah","A","B"),
+("Feedback","Conductoide","S","S"),
+("Atomico","Atomic Sapien","SS","SS"),
+("Bloxx","Segmentosapien","B","B"),
+("Fada-do-sono","Menuina","A","B"),
+("Espantoide","Espantosapien","B","B"),
+("O Pior","Atpociano","B","S"),
+("Escarabola","Antrotoide","A","B"),
+("Walkatruta","Trutoroide","A","B"),
+("Astrodactilo","Pterodiano","A","B"),
+("Bigotoide","Bigogisiano","A","B"),
+("Crocoroide","Galileano","A","B"),
+("Grillix","Orthoterrano","A","D"),
+("Gravattack","Galileano","A","SS"),
+("Podrao","Gasiceano","A","A"),
+("Vompiro","Vladat","A","S"),
+("Diabrete","Planchacule","A","B"),
+("AlienZ","Fischiciano","A","A"),
+("Shocksquath","Gimlinopithecus","A","A");
+
 select * from alien ;
 
 -- tabela 6
@@ -88,9 +154,27 @@ foreign key (fk_capitao) references encanador(idEncanador),
 nome varchar(45),
 idade int,
 raça varchar(45),
-serviço DATE
+serviço varchar(45)
 );
-insert into encanador values();
+insert into encanador (nome,idade,raça,serviço) values
+("kraion","75","vulpimancwer","Cao-de-caça",6),
+("Jasen","225","tetramando","Equipe-Tatica",6),
+("Burg","504","galvaniano","Engenheiro",null),
+("Kai","2","kinicelerano","Equipe-tatica",6),
+("Lr8161","20","mecamorfo","Armeiro",null),
+("Ttrax","54","petrosadien","Magestrado",7),
+("Lure","60","pisccis volann","Alto-Magestrado",null),
+("Garric","75","lepidopterrano","Equipe-Aerea",11),
+("Mathens","1023","ectonurita","Equipe-Aerea",11),
+("Bricer","574","pyronita","Magestrado",7),
+("Eloien","84","pelarota","Equipe-Aerea",11),
+("Droi","74","floranna","Equipe de reconhecimento"),
+("Mark","14","thep","Equipe de reconhecimento"),
+("Valbin","96","transyliano","Equipe de reconhecimento"),
+("Crayn","1174","mummyon","Magestrado",7),
+("Wolfkn","33","loboan","Comandante",15);
+
+
 select * from encanador ;
 
 -- tabela 7
@@ -104,6 +188,7 @@ fk_encanador int,
 foreign key (fk_encanador) references encanador(idEncanador)
 );
 insert into equipamento values();
+
 select * from equipamento ;
 
 -- tabela 8
@@ -120,7 +205,15 @@ select * from habilidade ;
 create table portador(
 idPortador int primary key auto_increment,
 nome varchar(45));
-insert into portador values();
+insert into portador (nome) values
+("Ben Tenyson"),
+("Evil Ben"),
+("Ben Negativo"),
+("Benzarro"),
+("Gwen 10"),
+("Ben 10.000"),
+("Ben 23");
+
 select * from portador ;
 
 -- tabela 10
